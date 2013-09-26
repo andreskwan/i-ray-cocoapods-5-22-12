2f2ff4903b11cfd5ba44de5a99fed9088f6775b8
ANDRES KWAN <andreskwan@gmail.com>
2013-09-26 01:19:11 -0500

    RAY - Obtainig JSON data about Mortal Kombat

--- CocoaPodsExample-5-22-12/KwaN_ViewController.m
+++ CocoaPodsExample-5-22-12/KwaN_ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "KwaN_ViewController.h"
+#import "TraktAPIClient.h"
+#import <AFNetworking.h>
 
 @interface KwaN_ViewController ()
 
@@ -14,10 +16,28 @@
 
 @implementation KwaN_ViewController
 
-- (void)viewDidLoad
-{
+-(void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // 1 - Create trakt API client
+    TraktAPIClient* client = [TraktAPIClient sharedClient];
+    // 2 - Create date instance with today's date
+    NSDate* today = [NSDate date];
+    // 3 - Create date formatter
+    NSDateFormatter* formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyyMMdd";
+    NSString* todayString = [formatter stringFromDate:today];
+    // 4 - Create API query request
+    NSString* path = [NSString stringWithFormat:@"user/calendar/shows.json/%@/%@/%@/%d", kTraktAPIKey, @"marcelofabri", todayString, 3];
+    NSURLRequest* request = [client requestWithMethod:@"GET" path:path parameters:nil];
+    // 5 - Create JSON request operation
+    AFJSONRequestOperation* operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+        // 6 - Request succeeded block
+        NSLog(@"%@", JSON);
+    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
+        // 7 - Request failed block
+    }];
+    // 8 - Start request
+    [operation start];
 }
 
 - (void)didReceiveMemoryWarning
